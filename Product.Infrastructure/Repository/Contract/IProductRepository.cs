bcc3243fc7661ede954687a7c741bf4aae53b63c
udodinho <udodinho200@gmail.com>
2024-07-04 01:41:26 +0100

    Create interface repository

---
+++ Product.Infrastructure/Repository/Contract/IProductRepository.cs
@@ -0,0 +1,8 @@
+﻿using Product.Domain.Entities;
+
+namespace Product.Infrastructure.Repository.Contract
+{
+    public interface IProductRepository : IRepository<ProductItem>
+    {
+    }
+}
---
+++ Product.Infrastructure/Repository/Contract/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq.Expressions;
+
+namespace Product.Infrastructure.Repository.Contract
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task<TEntity> GetByIdAsync(Guid id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity, bool>> predicate);
+        bool Exists(Expression<Func<TEntity, bool>> predicate);
+        Task AddAsync(TEntity entity);
+        void Remove(TEntity entity);
+        void Update(TEntity entity);
+        Task<TEntity> FirstOrDefaultNoTracking(Expression<Func<TEntity, bool>> predicate);
+        IQueryable<TEntity> QueryAll(Expression<Func<TEntity, bool>> predicate = null);
+        Task<TEntity> FirstOrDefaultAsync(Expression<Func<TEntity, bool>> predicate);
+    }
+}
