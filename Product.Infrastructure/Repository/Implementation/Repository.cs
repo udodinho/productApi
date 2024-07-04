c9e3808041b30d54668f51058735eaeeb2196475
udodinho <udodinho200@gmail.com>
2024-07-04 01:42:19 +0100

    Add repository

---
+++ Product.Infrastructure/Repository/Implementation/Repository.cs
@@ -0,0 +1,74 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Product.Domain.Entities;
+using Product.Infrastructure.Repository.Contract;
+
+namespace Product.Infrastructure.Repository.Implementation
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : BaseEntity
+    {
+        protected readonly ProductContext _context;
+
+        public Repository(ProductContext context)
+        {
+            _context = context;
+        }
+        public virtual async Task AddAsync(TEntity entity)
+        {
+            await _context.Set<TEntity>().AddAsync(entity);
+        }
+
+        public virtual bool Exists(Expression<Func<TEntity, bool>> predicate)
+        {
+            return _context.Set<TEntity>().Any(predicate);
+        }
+
+        public virtual async Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await _context.Set<TEntity>().Where(predicate).ToListAsync();
+        }
+
+        public virtual Task<TEntity> FirstOrDefaultNoTracking(Expression<Func<TEntity, bool>> predicate)
+        {
+            return _context.Set<TEntity>().AsNoTracking().FirstOrDefaultAsync(predicate);
+        }
+
+        public virtual Task<TEntity> FirstOrDefaultAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            return QueryAll().FirstOrDefaultAsync(predicate);
+        }
+
+        public virtual async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await _context.Set<TEntity>().ToListAsync();
+        }
+
+        public virtual async Task<TEntity> GetByIdAsync(Guid id)
+        {
+            var entity = await _context.Set<TEntity>().FindAsync(id);
+
+            if (entity == null) return null;
+
+            return entity;
+        }
+
+
+        public virtual void Remove(TEntity entity)
+        {
+            if (entity == null) throw new ArgumentNullException(nameof(entity));
+
+            _context.Set<TEntity>().Remove(entity);
+        }
+
+        public virtual void Update(TEntity entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public IQueryable<TEntity> QueryAll(Expression<Func<TEntity, bool>> predicate = null)
+        {
+            var query = _context.Set<TEntity>().OrderByDescending(x => x.CreatedAt).AsQueryable();
+            return predicate == null ? query : query.Where(predicate);
+        }
+    }
+}
