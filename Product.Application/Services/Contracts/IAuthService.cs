cb9e6bbbcef1652451b765d231efb7dc5779ab57
udodinho <udodinho200@gmail.com>
2024-07-04 01:31:51 +0100

    Add auth interface

---
+++ Product.Application/Services/Contracts/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using Application.Helper;
+using Product.Application.Dtos;
+
+namespace Product.Application.Services.Contracts
+{
+    public interface IAuthService
+    {
+        Task<SuccessResponse<LoginResponse>> Login(LoginDto model);
+        Task<SuccessResponse<RegisterResponse>> RegisterUser(RegisterDto model);
+    }
+}
