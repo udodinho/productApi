d70cfce6ce8a4459da96cbb343cd4810e43860cd
udodinho <udodinho200@gmail.com>
2024-07-04 01:42:36 +0100

    Add product repository

---
+++ Product.Infrastructure/Repository/Implementation/ProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using Product.Domain.Entities;
+using Product.Infrastructure.Repository.Contract;
+
+namespace Product.Infrastructure.Repository.Implementation
+{
+    public class ProductRepository : Repository<ProductItem>, IProductRepository
+    {
+        public ProductRepository(ProductContext appDbContext) : base(appDbContext)
+        {
+
+        }
+    }
+}
