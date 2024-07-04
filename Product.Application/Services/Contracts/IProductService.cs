e6aa21ed1e300e9f025a62601e342beb7efd6ff3
udodinho <udodinho200@gmail.com>
2024-07-04 01:32:11 +0100

    Add product interface

---
+++ Product.Application/Services/Contracts/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using Application.Helper;
+using Microsoft.AspNetCore.Mvc;
+using Product.Application.Dtos;
+using Product.Application.Helpers;
+
+namespace Product.Application.Services.Contracts
+{
+    public interface IProductService
+    {
+        Task<SuccessResponse<ProductsResponse>> CreateProduct(string userId, CreateProductDto model);
+        Task<SuccessResponse<ProductsResponse>> UpdateProduct(string userId, string productId, UpdateProductDto model);
+        Task<SuccessResponse<string>> DeleteProduct(string userId, string productId);
+        Task<SuccessResponse<ProductsResponse>> Product(string userId, string productId);
+        Task<PagedResponse<IEnumerable<ProductsResponse>>> Products(ResourceParameters parameter, string name, IUrlHelper urlHelper, string userId);
+    }
+}
