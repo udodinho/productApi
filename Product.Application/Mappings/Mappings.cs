4d63ea5a1adb1b0d46ebfc93840ebec13718e72a
udodinho <udodinho200@gmail.com>
2024-07-04 01:29:59 +0100

    Add automapper

---
+++ Product.Application/Mappings/Mappings.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Product.Application.Dtos;
+using Product.Domain.Entities;
+
+namespace Product.Application.Mappings
+{
+    public class Mappings : Profile
+    {
+        public Mappings()
+        {
+            CreateMap<CreateProductDto, ProductItem>();
+            CreateMap<RegisterDto, User>();
+            CreateMap<ProductItem, ProductsResponse>();
+
+
+        }
+    }
+}
