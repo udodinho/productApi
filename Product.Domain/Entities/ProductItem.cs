deb6c0c50c2e634d96b200395e69139948e2efa5
udodinho <udodinho200@gmail.com>
2024-07-04 01:37:19 +0100

    Create product entity

---
+++ Product.Domain/Entities/ProductItem.cs
@@ -0,0 +1,11 @@
+﻿namespace Product.Domain.Entities
+{
+    public class ProductItem : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string UserId { get; set; }
+        public virtual User User { get; set; }
+    }
+}
