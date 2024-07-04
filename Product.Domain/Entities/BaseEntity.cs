a92d5c964a7fac8f243ca871704885d50f6ed038
udodinho <udodinho200@gmail.com>
2024-07-04 01:37:03 +0100

    Create base entity

---
+++ Product.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace Product.Domain.Entities
+{
+    public class BaseEntity
+    {
+        public string Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
