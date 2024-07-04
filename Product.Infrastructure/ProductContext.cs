20603de81c5335a672ce71a56420795ed27a458d
udodinho <udodinho200@gmail.com>
2024-07-04 01:38:12 +0100

    Add product context

---
+++ Product.Infrastructure/ProductContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Product.Domain.Entities;
+
+namespace Product.Infrastructure
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProductItem> Products { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
