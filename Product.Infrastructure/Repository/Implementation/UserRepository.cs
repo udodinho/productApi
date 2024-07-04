59553705ebdf7d7c32161cdc86a88f94fc545b82
udodinho <udodinho200@gmail.com>
2024-07-04 01:42:48 +0100

    Add user repository

---
+++ Product.Infrastructure/Repository/Implementation/UserRepository.cs
@@ -0,0 +1,13 @@
+﻿using Product.Domain.Entities;
+using Product.Infrastructure.Repository.Contract;
+
+namespace Product.Infrastructure.Repository.Implementation
+{
+    public class UserRepository : Repository<User>, IUsersRepository
+    {
+        public UserRepository(ProductContext appDbContext) : base(appDbContext)
+        {
+
+        }
+    }
+}
