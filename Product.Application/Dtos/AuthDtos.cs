a45289b83f522e9fbdb6f9f28ffcb2a611a09f36
udodinho <udodinho200@gmail.com>
2024-07-04 01:28:14 +0100

    Add dtos

---
+++ Product.Application/Dtos/AuthDtos.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Product.Application.Dtos
+{
+    public record RegisterDto
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [MinLength(8)]
+        public string Password { get; set; }
+    }
+
+    public record LoginDto
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+
+    public record LoginResponse : RegisterResponse
+    {
+
+    }
+
+    public record RegisterResponse
+    {
+        public string Token { get; set; }
+        public string UserId { get; set; }
+    }
+
+}
---
+++ Product.Application/Dtos/ProductDto.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Product.Application.Dtos
+{
+    public record CreateProductDto
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+    }
+
+    public record CreateProductResponseDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Id { get; set; }
+        public decimal Price { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+
+    public record ProductsResponse : CreateProductResponseDto
+    {
+
+    }
+    public record UpdateProductDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
