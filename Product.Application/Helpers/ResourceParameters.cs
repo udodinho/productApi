3b8277f8f90601af272169a5e96c7bdb190f9609
udodinho <udodinho200@gmail.com>
2024-07-08 15:48:19 +0100

    fix: Add userId as filter parameter

--- Product.Application/Helpers/ResourceParameters.cs
+++ Product.Application/Helpers/ResourceParameters.cs
@@ -4,6 +4,7 @@
     {
         public int PageNumber { get; set; } = 1;
         public int PageSize { get; set; } = 10;
+        public string? UserId { get; set; }
         public string? Search { get; set; }
         public DateTime? StartDate { get; set; }
         public DateTime? EndDate { get; set; }
